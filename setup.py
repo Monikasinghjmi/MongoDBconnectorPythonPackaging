eb7f47dae412cb88689533edf34db06022343f90
Monika Singh <Monikasingh.jmi@gmail.com>
2024-12-19 18:25:53 +0530

    MONIKA

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 from setuptools import setup, find_packages
 from typing import List
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
-HYPEN_E_DOT='-e .'
-
-def get_requirement(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 __version__ = "0.0.1"
-REPO_NAME = "mongodbconnectorpkgMongoDBconnectorPythonPackaging"
+REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "MonikaSingh"
 AUTHOR_EMAIL = "monikasingh.jmi@gmail.com"
